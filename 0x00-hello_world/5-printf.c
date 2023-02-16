d86903a52dd6890b91fbe287011594a4fd5187b0
Ajiroba Mustapha <ajiroba980@gmail.com>
2023-02-16 15:57:29 -0800

    Print using print f

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#inclusw <stdio.h>
+/*
+ * main A program that print exactly with grammer
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return(0);
+}
