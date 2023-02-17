6d59e1b4d8903e587aefe70a7ab774fd4df01530
Ajiroba Mustapha <ajiroba980@gmail.com>
2023-02-16 16:19:35 -0800

    size of

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - A program that print the sizes of its intrinsic types
+ * Return: 0 (sucess)
+ */
+int main(void)
+{
+	int i;
+	char c;
+	long int l;
+	long long int t;
+	float f;
+
+printf("The size of int: %lu byte(s)\n", (unsigned long)sizeof(i));
+printf("The size of char: %lu byte(s)\n", (unsigned long)sizeof(c));
+printf("The size of ilong: %lu byte(s)\n", (unsigned long)sizeof(l));
+printf("The size of long long: %lu byte(s)\n", (unsigned long)sizeof(t));
+printf("The size of float: %lu byte(s)\n", (unsigned long)sizeof(f));
+return (0);
+}
