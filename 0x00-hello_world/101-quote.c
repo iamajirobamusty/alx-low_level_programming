620a08cc0aaff10ec8fe968c09fa3068d36d01c6
Ajiroba Mustapha <ajiroba980@gmail.com>
2023-02-16 17:15:13 -0800

    fixed

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - A program the print a quote to the standard error
- * Return: 1 (success)
+ * main - A program that prints a line to the standard output
+ * Return 1 (Sucess)
  */
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-return (1);
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n, stdout");
+retun (1);
 }
