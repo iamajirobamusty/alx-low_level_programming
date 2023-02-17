d701e14e1881d2fac6f19a338ea306d94ed4ee25
Ajiroba Mustapha <ajiroba980@gmail.com>
2023-02-16 16:23:20 -0800

    fixed

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,14 +5,14 @@
  */
 int main(void)
 {
-	int i;
 	char c;
+	int i;
 	long int l;
 	long long int t;
 	float f;
 
-printf("The size of int: %lu byte(s)\n", (unsigned long)sizeof(i));
 printf("The size of char: %lu byte(s)\n", (unsigned long)sizeof(c));
+printf("The size of int: %lu byte(s)\n", (unsigned long)sizeof(i));
 printf("The size of ilong: %lu byte(s)\n", (unsigned long)sizeof(l));
 printf("The size of long long: %lu byte(s)\n", (unsigned long)sizeof(t));
 printf("The size of float: %lu byte(s)\n", (unsigned long)sizeof(f));
