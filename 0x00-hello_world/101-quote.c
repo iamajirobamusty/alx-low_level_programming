d6b23845280139bf4e78dca391be311ef87c6039
Ajiroba Mustapha <ajiroba980@gmail.com>
2023-02-16 17:19:33 -0800

    fixed

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,10 @@
 #include <unistd.h>
 /**
  * main - A program that prints a line to the standard output
- * Return 1 (Sucess)
+ * Return: 1 (Sucess)
  */
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n, stdout");
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n, stdout");
 retun (1);
 }
