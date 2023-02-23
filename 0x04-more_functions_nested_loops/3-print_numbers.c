2b61a751389bcebb16f1bad7e32b7fc729ef72ba
Ajiroba Mustapha <ajiroba980@gmail.com>
2023-02-23 15:56:07 -0800

    print numer

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - print 0-9
+ * return: void
+ */
+
+void print_numbers(void)
+{
+	int i = 0;
+
+	while (i <= 9)
+	{
+		_putchar(i);
+	}
+	_putchar(\n);
+}
