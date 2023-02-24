0e7823aba6c6ab02b90823c7724535b4b1720d3d
Ajiroba Mustapha <ajiroba980@gmail.com>
2023-02-23 19:23:37 -0800

    fixed

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -2,25 +2,47 @@
 
 /**
  * print_number - prints an integer
- * @n: integer to be printed
+ * @n: random integer
+ * Return: void
  */
 void print_number(int n)
 {
-	unsigned int n1;
+	char ld, cr;
+	int r;
+	int ct = 0;
 
 	if (n < 0)
 	{
-		n1 = -n;
-		_putchar('-');
-	} else
-	{
-		n1 = n;
+		_putchar ('-');
+		ld = (char)('0' - (n % 10));
+		n = n / -10;
 	}
 
-	if (n1 / 10)
+	else
 	{
-		print_number(n1 / 10);
+		ld = (char)((n % 10) + '0');
+		n = n / 10;
 	}
 
-	_putchar((n1 % 10) + '0');
+	r = 0;
+	while (n > 0)
+	{
+		r = r * 10 + (n % 10);
+		n = n / 10;
+		ct++;
+	}
+
+	while (r > 0)
+	{
+		cr = (char)((r % 10) + '0');
+		_putchar(cr);
+		r = r / 10;
+		ct--;
+	}
+	while (ct != 0)
+	{
+		_putchar('0');
+		ct--;
+	}
+	_putchar(ld);
 }
