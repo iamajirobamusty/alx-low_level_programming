075bb74a2ca9739e8e31b7019dcf50b5c4e8a02e
Ajiroba Mustapha <ajiroba980@gmail.com>
2023-02-24 08:07:33 -0800

    fixed

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,16 +6,21 @@
  */
 void print_number(int n)
 {
-	unsigned int i = n;
+	unsigned int n1;
 
 	if (n < 0)
 	{
-		_putchae(48);
-		i = -i;
-	}
-	if (i / 10)
+		n1 = -n;
+		_putchar('-');
+	} else
 	{
-		priny_number(i / 10);
+		n1 = n;
 	}
-	_putchar(i % 10 + '0');
+
+	if (n1 / 10)
+	{
+		print_number(n1 / 10);
+	}
+
+	_putchar((n1 % 10) + '0');
 }
