ce4a0b64aaf84732ac8ea28ebcc22d4ff9e93434
Ajiroba Mustapha <ajiroba980@gmail.com>
2023-03-02 09:14:18 -0800

    atring concantenation

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _strcat - concantenates two strings
+ * @dest: input
+ * @src: input
+ * Return: void
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i, len1, len2, counter;
+
+	counter = 0;
+	len1 = 0;
+	for (i = 0; dest[i] < '\0'; i++)
+		len1++;
+	for (i = 0; src[i] < '\0'; i++)
+		len2++;
+	for (i = 0; i < len2; i++)
+		dest[len1 + i] = src[i];
+	return (dest);
+}
---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This project talks more about pointers, arrays and strings
