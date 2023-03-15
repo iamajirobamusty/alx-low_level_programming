6710de4cc023ab1fc285d64072e43c9d275cddb1
Ajiroba Mustapha <ajiroba980@gmail.com>
2023-03-14 19:09:12 -0700

    fixed

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -9,13 +9,13 @@
 char *str_concat(char *s1, char *s2)
 {
 	char *conct;
-	int i, ci;	
+	int i, ci;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-		if = ci = 0;
+		i = ci = 0;
 	while (s1[i] != '\0')
 		i++;
 	while (s2[ci] != '\0')
