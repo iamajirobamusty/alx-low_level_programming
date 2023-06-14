1e492577a0a84abe591311068812510b9b2126e6
iamajirobamusty <ajiroba980@gmail.com>
2023-06-14 16:21:39 -0700

    comparison error fixed

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,7 +13,7 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 	char *buffer;
 
-	if (size == NULL)
+	if (size == 0)
 		return (NULL);
 	buffer = malloc(size * sizeof(char));
 	for (i = 0; i < size; i++)
